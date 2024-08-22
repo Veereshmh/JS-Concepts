58394c4ac668e299e613ec3bb292377079fc987e
Veereshmh <veereshmh407@gmail.com>
2024-08-22 18:32:32 +0000

    container config done

---
+++ Basic_Javascript/01_Var.js
@@ -0,0 +1,23 @@
+const accountId = 232400
+let accountEmail = 'Veereshmh407@gmail.com'
+var accountPassword = '12345'
+accountCity = "Bengaluru"
+let accountState
+
+
+// accountId = 2 // Not Allowed
+accountEmail = 'Veereshmh8@gmail.com'
+accountPassword = '21212121'
+accountCity = 'Mysuru'
+
+// console.log(accountId);
+
+/*
+prefer not to use var
+Because of issue in block scope and functional scope
+
+*/
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ Basic_Javascript/02_DataTypes.js
@@ -0,0 +1,27 @@
+'use strict'; // Treat all JS Code as newer Version
+
+//alert (3 + 3) //We are using nodejs, Browser
+
+//console.log(3 + 3) // In Single Line Double console err: Unexcepted identifier 'console'
+//console.log('Veeresh')
+
+
+let name ='Veeresh'
+let age= 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ''
+// boolean => true/false
+// null => standalone value
+//undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+

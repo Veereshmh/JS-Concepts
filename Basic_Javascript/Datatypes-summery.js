ba5201d526e2cc33a124a70787a0d4409b5c78f5
Veereshmh <veereshmh407@gmail.com>
2024-08-23 16:10:15 +0000

    Datatypes - Summery

--- Basic_Javascript/Datatypes-summery.js
+++ Basic_Javascript/Datatypes-summery.js
@@ -2,6 +2,32 @@
 
 // 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const OutsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 34567893456789n
+ 
 
 // Reference (Non-Primitive)
 // Arrays, Objects, Functions
+
+const heros = ['Abc', 'Xyz', 'cde']
+
+let myObj = {
+    name: 'Veeresh',
+    age: 34
+}
+const myFunction = function() {
+    console.log("Hello World");
+    
+}
+console.log(typeof bigNumber);

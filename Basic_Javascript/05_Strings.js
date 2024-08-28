7c55b52b7c78a0a62b0b3008b510633d2ab2ca0a
Veereshmh <veereshmh407@gmail.com>
2024-08-28 17:43:41 +0000

    Strings

---
+++ Basic_Javascript/05_Strings.js
@@ -0,0 +1,23 @@
+const name = 'Veeresh' // String Declaration
+const repoCount = 50
+
+console.log(`Hi my name is ${name} and my repo count is ${repoCount}`);
+
+// one more method String Declaration
+
+const gameName = new String('Veereshmh')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('r'));
+
+
+const newString = gameName.substring(0,4)
+
+console.log(newString);
+
+
+

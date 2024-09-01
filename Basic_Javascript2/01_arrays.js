c447ff75388efdf34f5b6287154f08e2d057ade7
Veereshmh <veereshmh407@gmail.com>
2024-09-01 18:35:14 +0000

    Arrays

---
+++ Basic_Javascript2/01_arrays.js
@@ -0,0 +1,15 @@
+// arrays
+
+const myArr = [0,1,2,3,4,5]
+
+const myHeros = ['Veeresh', 'Xyz']
+
+const myArr2 = new  Array(1,2,3,4)
+console.log(myArr[1]);
+
+// Array Methods
+
+myArr.push(6) // Add the Element in the Array
+myArr.pop() // Remove the last Element in tha array
+
+console.log(myArr);

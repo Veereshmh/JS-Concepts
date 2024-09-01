971bc6f060c15c6f00f54ad2d9850d455ec913a8
Veereshmh <veereshmh407@gmail.com>
2024-09-01 09:17:27 +0000

    Strings

--- Basic_Javascript/05_Strings.js
+++ Basic_Javascript/05_Strings.js
@@ -5,7 +5,7 @@ console.log(`Hi my name is ${name} and my repo count is ${repoCount}`);
 
 // one more method String Declaration
 
-const gameName = new String('Veereshmh')
+const gameName = new String('Veeresh-mh-com')
 
 console.log(gameName[0]);
 console.log(gameName.__proto__);
@@ -18,6 +18,23 @@ console.log(gameName.indexOf('r'));
 const newString = gameName.substring(0,4)
 
 console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   Veeresh Hiremath    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://www.flipkart.com/mobile%20phones"
+console.log(url.replace('%20', '-'));
+console.log(url.includes('stores'))
+
+console.log(gameName.split('-'));
+
+
+
+
 
 
 

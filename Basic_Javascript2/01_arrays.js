b5224470162b0e9448e9c3fb36f28e0a5d111653
Veereshmh <veereshmh407@gmail.com>
2024-09-02 08:14:54 +0000

    Arrays

--- Basic_Javascript2/01_arrays.js
+++ Basic_Javascript2/01_arrays.js
@@ -12,4 +12,33 @@ console.log(myArr[1]);
 myArr.push(6) // Add the Element in the Array
 myArr.pop() // Remove the last Element in tha array
 
+myArr.unshift(9)
+myArr.shift()
+
 console.log(myArr);
+console.log(myArr.includes(9));
+
+
+const newArr = myArr.join()
+
+console.log(newArr);
+
+
+// slice splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log("c", myArr);
+
+console.log(myn2);
+
+
+
+
+
+

9426f4a8a8934d82362e4a5bd67cbc17610163f7
Veereshmh <veereshmh407@gmail.com>
2024-09-02 16:51:04 +0000

    Objects Part2

--- Basic_Javascript2/04_Objects.js
+++ Basic_Javascript2/04_Objects.js
@@ -26,6 +26,17 @@ const obj2 = {3: 'a', 4: 'b'}
 
 // const obj3 = { obj1, obj2 }
 
-const obj3 = Object.assign(obj1, obj2)
+// const obj3 = Object.assign(obj1, obj2)
 
-console.log(obj3);
+const obj3 = {...obj1, ...obj2}
+
+// console.log(obj3);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+

3082806517df4d877990fa4562bf38011f8ee088
Veereshmh <veereshmh407@gmail.com>
2024-09-13 18:29:36 +0000

    Scope and Nested

--- Basic_Javascript3/02_Scopes.js
+++ Basic_Javascript3/02_Scopes.js
@@ -9,10 +9,52 @@ if(true){
 const b = 20;  //// Block Scope
 c = 30;
 
-console.log("Inner value of a:", a);
+// console.log("Inner value of a:", a);
 
 }
 
-console.log("Outer value of a:", a);
+// console.log("Outer value of a:", a);
 // console.log(b);
 // console.log(c);
+
+
+// Nested Scopes
+
+function One ()
+{
+    const username = "Veeresh"
+
+    function two (){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    //console.log(website);
+
+    two()
+    
+}
+// One()
+
+if(true){
+    const username = "Veeresh123"
+    if(username === 'Veeresh123'){
+        const website="google"
+        // console.log(username + website);        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+// ++++++++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++++++++++++++  
+
+console.log(addOne(5)) //  Cann access 'addOne' before initialization
+function addOne(num){
+    return num + 1
+}
+
+addTwo(5)  //  Cannot access 'addTwo' before initialization
+const addTwo = function(num){
+    return num + 2
+}
+ 

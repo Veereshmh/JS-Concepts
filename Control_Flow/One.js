63fb0b10e07f3e8e272d4c056859494bd5c7c37c
Veereshmh <veereshmh407@gmail.com>
2024-09-26 15:30:35 +0000

    Control Flow

--- Control_Flow/One.js
+++ Control_Flow/One.js
@@ -5,11 +5,39 @@
 // == Comparision the value
 // === check the data type of value
 
-const isUserLoggedIn = true
-const temprature = 51
+// const isUserLoggedIn = true
+// const temprature = 51
 
-if(temprature < 50){
-    console.log("temprature is less than 50");    
-}else {
-    console.log("temprature is greater than 50")
+// if(temprature < 50){
+//     console.log("temprature is less than 50");    
+// }else {
+//     console.log("temprature is greater than 50")
+// }
+
+
+// const score = 200
+
+// if(score > 100){
+//     var power = "fly"
+//     console.log(`User Power ${power}`);
+// }
+// console.log(`User power ${power}`);
+
+const balance = 1000 
+//  if(balance > 500) console.log("Test");  
+
+// Nesting
+
+if (balance < 500){
+    console.log("less than 500");
+} else if (balance < 750){
+    console.log("less than 750");
+} else if (balance < 900){
+    console.log("less than 900");
+} else {
+    console.log(`Balance is less than 1200 your current balance is ${balance}`);
 }
+
+
+
+const userLoggedIn = true

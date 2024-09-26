d9ac603664a15c593de07aec8216ed280bb31ec6
Veereshmh <veereshmh407@gmail.com>
2024-09-26 13:36:37 +0000

    Control Flow

---
+++ Control_Flow/One.js
@@ -0,0 +1,15 @@
+// if 
+
+
+// = single Equal id assignment operator
+// == Comparision the value
+// === check the data type of value
+
+const isUserLoggedIn = true
+const temprature = 51
+
+if(temprature < 50){
+    console.log("temprature is less than 50");    
+}else {
+    console.log("temprature is greater than 50")
+}

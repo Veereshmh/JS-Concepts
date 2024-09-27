b6e1909a666a9463ac7ee6c82e717becd5d75585
Veereshmh <veereshmh407@gmail.com>
2024-09-27 04:38:13 +0000

    For loop and while

--- Iterations5/One.js
+++ Iterations5/One.js
@@ -28,3 +28,26 @@ for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
     console.log(element);
     }
+
+
+
+    // break and continue
+
+    for (let index = 1; index <= 20; index++) {
+        if (index == 5){
+            console.log("Detected 5");
+            break
+        }
+        console.log(`Value of i is ${index}`); 
+    }
+
+    // Continue
+
+
+    for (let index = 1; index <= 20; index++) {
+        if (index == 5){
+            console.log("Detected 5");
+            continue
+        }
+        console.log(`Value of i is ${index}`); 
+    }

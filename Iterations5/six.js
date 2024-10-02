94c65d18e7c22d345785e39ed6d67a6e709f8ad9
Veereshmh <veereshmh407@gmail.com>
2024-10-02 14:16:16 +0000

    flter

--- Iterations5/six.js
+++ Iterations5/six.js
@@ -11,3 +11,73 @@ const myNums = [1,2,3,4,5,6,7,8,9,10]
 const newNums = myNums.filter((num) => num > 4)
 
 console.log(newNums);
+
+
+const newnums1 = myNums.filter((num) => {
+    return num > 4
+})
+console.log(newnums1);
+
+
+const EmptyArr = []
+
+myNums.forEach( (num) => {
+    if (num > 3){
+        EmptyArr.push(num)
+    }
+})
+console.log(EmptyArr);
+
+
+const Books = [
+    {   title:'Book One', 
+        genre:'science',
+        publish:1990, 
+        Edition: 2003
+    },
+    {   title:'Book Two', 
+        genre:'Politcs',
+        publish:2012, 
+        Edition: 2005
+    },
+    {   title:'Book Three', 
+        genre:'Maths',
+        publish:1991, 
+        Edition: 2007
+    },
+    {   title:'Book Four', 
+        genre:'Kannada',
+        publish:1994, 
+        Edition: 2006
+    },
+    {   title:'Book Five', 
+        genre:'English',
+        publish:1997, 
+        Edition: 2010
+    },
+    {   title:'Book Six', 
+        genre:'social',
+        publish:1999, 
+        Edition: 2020
+    },
+    {   title:'Book Seven', 
+        genre:'Medicne',
+        publish:2000, 
+        Edition: 2021
+    },
+    {   title:'Book Eight', 
+        genre:'Hindi',
+        publish:2010, 
+        Edition: 2023
+    },
+    {   title:'Book Nine', 
+        genre:'Bialogy',
+        publish:2001, 
+        Edition: 2024
+    }
+]
+
+const UserBokks = Books.filter((bk) =>bk.genre === 'social' )
+const UserBokks1 = Books.filter((bk) => { bk.publish >= 2000})
+console.log(UserBokks);
+console.log(UserBokks1);

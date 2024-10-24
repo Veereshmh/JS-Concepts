63171c550c143e09132c55874253c56715f3bfb7
Veereshmh <veereshmh407@gmail.com>
2024-10-24 18:19:54 +0000

    call

---
+++ Classes_Oop10/Prototype.js
@@ -0,0 +1,22 @@
+let myName = "Veeresh"
+
+// console.log(myName.length);
+// console.log(myName.truelength);
+// console.log(myName.trim().length);
+
+
+let myHeros = ['thor', 'spiderman']
+
+let heyopower = {}
+    Object.prototype.Veeresh = function(){
+        console.log(`Veeresh is present in all objects`);
+        
+    }
+    
+    Array.prototype.heyVeeresh = function(){
+        console.log(`Veeersh Says Hello`);
+        
+    }
+myHeros.Veeresh();
+myHeros.heyVeeresh();
+// heyoPower.heyVeeresh()

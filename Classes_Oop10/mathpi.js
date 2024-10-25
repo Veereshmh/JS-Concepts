adb9be2a6eb4c99ed4b8a4cbc9b8fc403fefb6c8
Veereshmh <veereshmh407@gmail.com>
2024-10-25 05:12:25 +0000

    mathpi.js

---
+++ Classes_Oop10/mathpi.js
@@ -0,0 +1,12 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math,"PI")
+
+// console.log(descriptor);
+
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true
+}
+
+console.log(Object);
+
